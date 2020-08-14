a55b8d548ad039047f73849ae226e0d35c0be22e
szmoon <szmoon@gmail.com>
2020-08-13 21:27:23 -0700

    drag pass 1 works

--- src/components/atoms/window/index.vue
+++ src/components/atoms/window/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="a-window" :aria-label="aria">
+  <div class="a-window" id="mydiv" :aria-label="aria">
     <!-- top bar -->
-    <div class="a-window__top-bar">
+    <div class="a-window__top-bar" id="mydivheader">
       {{ name }}
       <!-- close button -->
       <div class="a-window__close-button" aria-label="close button">
@@ -37,7 +37,60 @@ export default {
       required: true
     }
   },
-  computed: {}
+  data() {
+    return {
+      pos1: 0,
+      pos2: 0,
+      pos3: 0,
+      pos4: 0,
+      elmnt: undefined
+    };
+  },
+  computed: {},
+  mounted() {
+    this.dragElement(document.getElementById('mydiv'));
+    this.elmnt = document.getElementById('mydiv');
+  },
+  methods: {
+    dragElement(elmnt) {
+      if (document.getElementById(elmnt.id + 'header')) {
+        // if present, the header is where you move the DIV from:
+        document.getElementById(
+          elmnt.id + 'header'
+        ).onmousedown = this.onMouseDown;
+      } else {
+        // otherwise, move the DIV from anywhere inside the DIV:
+        elmnt.onmousedown = this.onMouseDown;
+      }
+    },
+    onMouseDown(e) {
+      e = e || window.event;
+      e.preventDefault();
+      // get the mouse cursor position at startup:
+      this.pos3 = e.clientX;
+      this.pos4 = e.clientY;
+      document.onmouseup = this.closeDragElement;
+      // call a function whenever the cursor moves:
+      document.onmousemove = this.elementDrag;
+    },
+    closeDragElement() {
+      // stop moving when mouse button is released:
+      document.onmouseup = null;
+      document.onmousemove = null;
+    },
+    elementDrag(e) {
+      e = e || window.event;
+      e.preventDefault();
+      // calculate the new cursor position:
+      this.pos1 = this.pos3 - e.clientX;
+      this.pos2 = this.pos4 - e.clientY;
+      this.pos3 = e.clientX;
+      this.pos4 = e.clientY;
+      // set the element's new position:
+      this.elmnt.style.top = this.elmnt.offsetTop - this.pos2 + 'px';
+      this.elmnt.style.left = this.elmnt.offsetLeft - this.pos1 + 'px';
+    }
+  }
 };
 </script>
 
